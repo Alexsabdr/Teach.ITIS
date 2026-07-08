ff6dfa5370ecb1ddb40a6bc624db8393a07dadd8
Kamil Mingaliev <kamilmingaliev0@gmail.com>
2026-07-08 20:46:11 +0300

    Rename Django project package to config

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for teach_itis project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
