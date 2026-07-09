6089b30395f8dce3947d49505d8b54e8cd6f8256
Amir Akhmadullin <amir_akhmadullin9@mail.ru>
2026-07-09 18:17:21 +0300

    Configure Django admin for stage 4

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Discipline, Topic, Enrollment
+
+
+@admin.register(Discipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "slug",
+        "created_by",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "slug",
+        "description",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "discipline",
+        "order_number",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "slug",
+        "description",
+        "discipline__title",
+    )
+
+    list_filter = (
+        "discipline",
+        "created_at",
+    )
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "discipline",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "discipline__title",
+    )
+
+    list_filter = (
+        "discipline",
+        "created_at",
+    )
